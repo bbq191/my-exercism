861a9b88021e579d94ea3a1924b184b7db25aea9
Vinci Xu <xsiyuan@gmail.com>
2023-09-29 14:42:20 +0800

    add: gigasecond

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
+use std::time::Duration;
+
 use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    start + Duration::from_secs(1_000_000_000)
 }
